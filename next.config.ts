d87e578a8559e99f2f56e6bac8366455e02cc939
Andreas Lukita <andreas030503@gmail.com>
2024-12-08 15:33:57 +0800

    Update the News component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.cnbcfm.com",
+        port: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "a57.foxnews.com",
+        port: "**",
+      },
+    ],
+    domains: ["image.cnbcfm.com", "a57.foxnews.com"],
+  },
 };
 
 export default nextConfig;
