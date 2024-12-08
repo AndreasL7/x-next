3cad2a3ea12fb1a37ad20fc03fa05ababa86092d
Andreas Lukita <andreas030503@gmail.com>
2024-12-08 14:42:43 +0800

    Modify the sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex justify-between max-w-6xl mx-auto">
-          <div>
+          <div className="hidden sm:inline border-r h-screen">
             <Sidebar />
           </div>
           <div>{children}</div>
